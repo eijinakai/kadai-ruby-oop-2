d14c7c35677a8edb75fabe58b840fc3b6b69526e
eiji.nakai <nakai_7254@yahoo.co.jp>
2022-06-12 23:08:22 +0000

    kadai-ruby-oop2-syusei_1 comit

--- mentor.rb
+++ mentor.rb
@@ -16,13 +16,11 @@ class Mentor
 end
 
 #煌木（変数名：kirameki）という名前を持つ Mentor クラスのインスタンスを生成してinitializeで名前を初期化。
-Name1 = Mentor.new('煌木')
+mentor_name1 = Mentor.new('煌木')
 
 #RailsMentorクラスの定義とMentorクラスを継承するため、「class RailsMentor < Mentor」と記述した。
 class RailsMentor < Mentor
-  def initialize(name)
-   self.name =name
-  end
+  
 #class RailsMentor < mentorの記載に加え、メソッド名を変更しないことで、job メソッドをオーバーライドしインスタンス変数の値を
 #使って 「○○です。私はRubyとRailsでWebアプリケーションを作ります。 」と表示するメソッドにした。
   def job
@@ -31,9 +29,9 @@ class RailsMentor < Mentor
 end
 
 #赤出（変数名：akaide）という名前を持つ RailsMentor クラスのインスタンスを生成してinitializeで名前を初期化。
-Name2 = RailsMentor.new('赤出')
+mentor_name2 = RailsMentor.new('赤出')
 
 #jobの呼び出し
-Name1.job
-Name2.job
+mentor_name1.job
+mentor_name2.job
 
